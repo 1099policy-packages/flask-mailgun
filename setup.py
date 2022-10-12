1630d58fe9b2efb59ec6e7899eb18416551d6548
Cemre Fatih Karakulak <cradexco@gmail.com>
2022-10-12 13:49:17 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import platform
+from pathlib import Path
+
+from setuptools import find_packages, setup
+from distutils.version import StrictVersion
+
+
+if StrictVersion(platform.python_version()) < StrictVersion('3.0.0'):
+    raise Exception("`t99-mailgun` support python version >= 3.0.0 only.")
+
+setup(
+    name='t99-mailgun',
+    version='21.12.08',
+    description=(
+        'Flask Mailgun for Python web framework'
+    ),
+    long_description=Path(__file__).parent.joinpath('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    package_data={
+        't99': ['mailgun/*'],
+    },
+    install_requires=[
+    ],
+    url='https://github.com/1099policy-packages/flask-mailgun',
+    author='Ray Ventura',
+    author_email='ray@1099policy.com',
+)
